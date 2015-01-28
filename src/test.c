1b3457a52562e392e8cb5374388a49f4e1d4a15a
swfrench <scott.w.french@gmail.com>
2015-01-27 23:58:35 -0800

    Added timeout to test case
    After some number of seconds (arbitrary timeout) waiting for the lock,
    the test will assume deadlock and abort.

--- src/test.c
+++ src/test.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <assert.h>
+#include <signal.h>
 #include <mpi.h>
 
 #include "rma_mutex.h"
+
+/* lock wait timeout (seconds) before assuming deadlock */
+#define DEADLOCK_TIMEOUT 5
+
+#define with_timeout(m, t, f) do { \
+  set_timer(m, t); \
+  f \
+  alarm(0); \
+} while (0);
+
+struct {
+  int len;
+  char *msg;
+} timeout_msg;
+
+void timeout_handler(int sig)
+{
+  write(STDERR_FILENO, timeout_msg.msg, timeout_msg.len);
+  exit(EXIT_FAILURE);
+}
+
+void set_timer(const char *msg, int t)
+{
+  timeout_msg.len = strlen(msg);
+  timeout_msg.msg = malloc(timeout_msg.len + 1);
+  assert(timeout_msg.msg != NULL);
+  strcpy(timeout_msg.msg, msg);
+  signal(SIGALRM, timeout_handler);
+  alarm(t);
+}
 
 int main(int argc, char **argv)
 {
@@ -13,17 +46,21 @@ int main(int argc, char **argv)
 
   rma_mutex_init(&m, MPI_COMM_WORLD, 0);
 
-  /* drop some ranks, in an effort to detect incorrect lock assignee
-   * indexing (which can be missed if all ranks contending for lock) */
-  if (m.rank % 3 != 0) {
-    rma_mutex_lock(&m);
+  /* start deadlock timer */
+  with_timeout("error: timeout in lock acquisition; assuming deadlock\n",
+    DEADLOCK_TIMEOUT, {
+      /* drop some ranks, in an effort to detect incorrect lock assignee
+       * indexing (which can be missed if all ranks contending for lock) */
+      if (m.rank % 3 != 0) {
+        rma_mutex_lock(&m);
 
-    printf("%3.3i has lock\n", m.rank); fflush(stdout);
+        printf("%3.3i has lock\n", m.rank); fflush(stdout);
 
-    printf("%3.3i about to release lock\n", m.rank); fflush(stdout);
+        printf("%3.3i about to release lock\n", m.rank); fflush(stdout);
 
-    rma_mutex_unlock(&m);
-  }
+        rma_mutex_unlock(&m);
+      }
+    });
 
   rma_mutex_free(&m);
   
